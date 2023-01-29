131e696d78de893824d2b9ddb45bf65b223a1bda
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-01-29 04:20:34 -0700

    add number values to radio buttons to be parseInt() back to a value

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-document.querySelector('input[name=q1]:checked').value
-document.querySelector('input[name=q2]:checked').value
-document.querySelector('input[name=q3]:checked').value
-document.querySelector('input[name=q4]:checked').value
-document.querySelector('input[name=q5]:checked').value
+document.querySelector('input[name=q2]:checked').value;
+document.querySelector('input[name=q3]:checked').value;
+document.querySelector('input[name=q4]:checked').value;
+document.querySelector('input[name=q5]:checked').value;
+
+document.getElementById(survey).submit()
+
+function acccumulateAnswers() {
+
+}
+
+function handleRadio(event) {
+  event.preventDefault();
+  const radioSelection = document.querySelector('input[name=q1]:checked').value;
+  return radioSelection
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("radio-form").addEventListener("submit", handleRadio);
+})

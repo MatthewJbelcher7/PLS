b4e98092ec0f020397bdac7d18f1e3593ed15333
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-01-29 03:59:07 -0700

    begin JS

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('input[name=q1]:checked').value
+document.querySelector('input[name=q2]:checked').value
+document.querySelector('input[name=q3]:checked').value
+document.querySelector('input[name=q4]:checked').value
+document.querySelector('input[name=q5]:checked').value

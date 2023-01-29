b74cfb6b106baed1de01b6500f6c75f81be92f5f
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-01-29 05:46:07 -0700

    add javascript code up until I got a little confused

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
-function handleRadio(event) {
-  event.preventDefault();
-  const radioS1 = parseInt(document.querySelector('input[name=q1]:checked').value);
-  const radioS2 = parseInt(document.querySelector('input[name=q2]:checked').value);
-  const radioS3 = parseInt(document.querySelector('input[name=q3]:checked').value);
-  const radioS4 = parseInt(document.querySelector('input[name=q4]:checked').value);
-  const radioS5 = parseInt(document.querySelector('input[name=q5]:checked').value);
-  function acccumulateAnswers() {
-    const radioSelection = (radioS1 + radioS2 + radioS3 + radioS4 + radioS5) / 5;
-    return radioSelection;
-  }
-  
-  if (radioSelection = 1) {
-    removeAttribute
-  }
-  const finalSelection = acccumulateAnswers();
-  return finalSelection;
+// User Interface Logic
+
+function hideResults() {
+  document.getElementById("Sharp").setAttribute("class", "hidden");
+  document.getElementById("JavaS").setAttribute("class", "hidden");
+  document.getElementById("Python").setAttribute("class", "hidden");
+  document.getElementById("Ruby").setAttribute("class", "hidden");
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
-})
+window.onload = function() {
+  hideResults();
+
+  document.querySelector("radio-form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResults();
+    const radioS1 = parseInt(document.querySelector('input[name=q1]:checked').value);
+    const radioS2 = parseInt(document.querySelector('input[name=q2]:checked').value);
+    const radioS3 = parseInt(document.querySelector('input[name=q3]:checked').value);
+    const radioS4 = parseInt(document.querySelector('input[name=q4]:checked').value);
+    const radioS5 = parseInt(document.querySelector('input[name=q5]:checked').value);
+      function acccumulateAnswers() {
+        const radioSelection = Math.round(radioS1 + radioS2 + radioS3 + radioS4 + radioS5) / 5;
+        return radioSelection;
+      }
+    
+      if (radioSelection = 1) {
+        document.getElementById("Sharp").removeAttribute("class");
+      } else if (radioSelection = 2) {
+        document.getElementById("JavaS").removeAttribute("class");
+      } else if (radioSelection = 3) {
+        document.getElementById("Python").removeAttribute("class");
+      } else if (radioSelection = 4) {
+        document.getElementById("Ruby").removeAttribute("class");
+      }
+
+    const finalSelection = acccumulateAnswers();
+    return finalSelection;
+  };
+};
+
+// window.addEventListener("load", function() {
+//   document.getElementById("radio-form").addEventListener("submit", handleRadio);
+// })

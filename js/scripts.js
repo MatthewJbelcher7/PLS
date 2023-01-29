e0f8af41328396db4d9d4d0262da353feb70ee01
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-01-29 05:10:51 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
-document.querySelector('input[name=q2]:checked').value;
-document.querySelector('input[name=q3]:checked').value;
-document.querySelector('input[name=q4]:checked').value;
-document.querySelector('input[name=q5]:checked').value;
-
-document.getElementById(survey).submit()
-
-function acccumulateAnswers() {
-
-}
-
 function handleRadio(event) {
   event.preventDefault();
-  const radioSelection = document.querySelector('input[name=q1]:checked').value;
-  return radioSelection
+  const radioS1 = parseInt(document.querySelector('input[name=q1]:checked').value);
+  const radioS2 = parseInt(document.querySelector('input[name=q2]:checked').value);
+  const radioS3 = parseInt(document.querySelector('input[name=q3]:checked').value);
+  const radioS4 = parseInt(document.querySelector('input[name=q4]:checked').value);
+  const radioS5 = parseInt(document.querySelector('input[name=q5]:checked').value);
+  function acccumulateAnswers() {
+    const radioSelection = (radioS1 + radioS2 + radioS3 + radioS4 + radioS5) / 5;
+    return radioSelection;
+  }
+  
+  if (radioSelection = 1) {
+    removeAttribute
+  }
+  const finalSelection = acccumulateAnswers();
+  return finalSelection;
 }
 
 window.addEventListener("load", function() {

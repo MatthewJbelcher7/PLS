6373870172227c28168fde6d2aac471640933440
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-02-06 15:12:39 -0700

    add resolution to html for broken Math.round

--- js/scripts.js
+++ js/scripts.js
@@ -21,17 +21,19 @@ window.onload = function() {
     const radioS3 = parseInt(document.querySelector('input[name=q3]:checked').value);
     const radioS4 = parseInt(document.querySelector('input[name=q4]:checked').value);
     const radioS5 = parseInt(document.querySelector('input[name=q5]:checked').value);
-    console.log(radioS1)
 
-      const radioSelection = Math.round(radioS1 + radioS2 + radioS3 + radioS4 + radioS5) / 5;
-      if (radioSelection === 1) {
-        document.getElementById("Sharp").removeAttribute("class");
+      const radioSelection = Math.round((radioS1 + radioS2 + radioS3 + radioS4 + radioS5) / 5);
+
+      if (radioSelection === null) {
+        document.getElementById("Error").removeAttribute("class");
       } else if (radioSelection === 2) {
         document.getElementById("JavaS").removeAttribute("class");
       } else if (radioSelection === 3) {
         document.getElementById("Python").removeAttribute("class");
       } else if (radioSelection === 4) {
         document.getElementById("Ruby").removeAttribute("class");
+      } else if (radioSelection === 1){
+        document.getElementById("Sharp").removeAttribute("class");
       }
   };
 };
